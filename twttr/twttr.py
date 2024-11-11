86adc14ef3842555c12ec2f722dd672c643df056
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-11 08:33:04 +0000

    Mon, Nov 11, 2024, 4:33 PM +08:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,7 +1,7 @@
 string = input("Input:")
 twttr =""
 for i in string:
-    if i not in ['a', 'e', 'i' ,'o', 'u']:
+    if i not in ['a', 'e', 'i' ,'o', 'u', 'A', 'E', 'I', 'O', 'U']:
         twttr = twttr + i
 print(twttr)
 
