650d8804e9a00b5836ea3f169b49b41830351d8f
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-11 03:23:24 +0000

    Mon, Nov 11, 2024, 11:23 AM +08:00

--- coke/coke.py
+++ coke/coke.py
@@ -3,13 +3,16 @@ price = 50
 print("Amount Due: ", price)
 coin = input("Insert Coin: ")
 
-if coin in [25,10,5]:
-    receive += coin
-    if price > receive:
-        print("Insert Coin: ", price-receive)
-        coin = input("Insert Coin: ")
+while receive < price:
+    if coin in [25,10,5]:
         receive += coin
+        if price > receive:
+            print("Insert Coin: ", price-receive)
+            coin = input("Insert Coin: ")
+            receive += coin
+        else:
+            print("Change Owed: ", receive - price)
+            coin = input("Insert Coin: ")
     else:
-        print("Change Owed: ", receive - price)
-else:
-    print("Amount Due: ", price - receive)
+        print("Amount Due: ", price - receive)
+
