61b12a270a76237ff50a3e0c63c9f84f753897eb
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-11 09:13:15 +0000

    Mon, Nov 11, 2024, 5:13 PM +08:00

--- plates/plates.py
+++ plates/plates.py
@@ -7,15 +7,30 @@ def main():
 
 
 def is_valid(s):
-    '''
-    “All vanity plates must start with at least two letters.”
-    “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
-    “Numbers cannot be used in the middle of a plate; they must come at the end.
-    For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
-    “No periods, spaces, or punctuation marks are allowed.”
-    '''
-    if len(s)
-    ...
 
+    #    “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
+    if len(s) > 6 or len(s) < 2:
+        return False
+    #“All vanity plates must start with at least two letters.”
+    for i in s[0: 2]:
+        if i.isdigit():
+            return False
+    #“No periods, spaces, or punctuation marks are allowed.”
+    if not s.isalnum():
+        return False
+    #“Numbers cannot be used in the middle of a plate; they must come at the end.
+    #For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
+    cout = 0
+    if s.isalpha():
+        return True
+    for j in s:
+        cout += 1
+        if j.isdigit():
+            if j == "0":
+                return False
+            elif s[cout:].isdigit():
+                return True
+            else:
+                return False
 
 main()
