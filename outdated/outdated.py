3a1f0dc1d1541bb84e7638ba060ecd9add75c49d
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-12 11:37:47 +0000

    Tue, Nov 12, 2024, 7:37 PM +08:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,3 +1,20 @@
+month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
 while True:
     date = input("Date:")
-    
+    if '/' in date:
+        m, d, y = date.split('/')
+        print(f"{y:04}-{m:02}-{d:02}")
+
