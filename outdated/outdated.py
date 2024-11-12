26a85852663f34a46aa69be84d37fe021cd06820
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-12 11:49:56 +0000

    Tue, Nov 12, 2024, 7:49 PM +08:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -21,14 +21,19 @@ while True:
     elif ',' in date:
         md, y = date.split(', ')
         m, d = md.split(' ')
-        n=0
-        for i in month:
-            n+1
-            if i == m:
-                break
-        print(f"{y:04}-{int(m):02}-{int(d):02}")
-        break
+        if m in month:
+            n=0
+            for i in month:
+                n+1
+                if i == m:
+                    break
+            print(f"{y:04}-{int(m):02}-{int(d):02}")
+            break
+        else:
+            pass
+
     else:
+        pass
 
 
 
