644b58228d4294f4056e2e0911cfe972e1519c36
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-12 11:23:23 +0000

    Tue, Nov 12, 2024, 7:23 PM +08:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -2,7 +2,7 @@ grcry = {}
 while True:
     try:
         item = input()
-        grcry[item.upper()] = None
+        grcry[item.upper()] = grcry
     except EOFError:
         for i in sorted(grcry):
             print(i)
