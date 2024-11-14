4d87b6fb78b5bc0cc86a9736cdfeafb95cd51af9
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-14 03:04:09 +0000

    Thu, Nov 14, 2024, 11:04 AM +08:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -3,5 +3,11 @@ from pyfiglet import Figlet
 
 figlet = Figlet()
 
-if len(argv) == 0:
-    
+if len(argv) == 1:
+    txt = input("Input:")
+elif len(argv) ==3:
+    ...
+else:
+    print("Invalid usage")
+    sys.exit()
+
