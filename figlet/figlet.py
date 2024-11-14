d9623989965275693db115a3ccfd95952fb2aef5
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-14 03:19:29 +0000

    Thu, Nov 14, 2024, 11:19 AM +08:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -9,7 +9,7 @@ if len(sys.argv) == 1:
     txt = input("Input:")
     print(figlet.renderText(txt))
 elif len(sys.argv) ==3:
-    if sys.argv[1] == '-f' and sys.argv[2] in fonts:
+    if (sys.argv[1] == '-f' or sys.argv[1] == '--font') and sys.argv[2] in fonts:
         txt = input("Input:")
         figlet.setFont(font=sys.argv[2])
         print(figlet.renderText(txt))
