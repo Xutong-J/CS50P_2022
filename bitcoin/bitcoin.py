99e48a2b1235090be1111b5ca1e9a8812739c3e6
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-15 11:06:45 +0000

    Fri, Nov 15, 2024, 7:06 PM +08:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -2,9 +2,13 @@ import requests
 import sys
 
 if len(sys.argv) == 1:
-    print("Missing command-line argument")
-    sys.exit()
-if sys.argv[1].isdigit()
+    sys.exit("Missing command-line argument")
+
+try:
+    n = float(sys.argv[1])
+    
+except ValueError:
+    sys.exit("Command-line argument is not a number")
 
 
 try:
