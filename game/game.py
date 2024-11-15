87f1369b26b80a5364456b559a49a293eb2e024e
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-15 09:07:46 +0000

    Fri, Nov 15, 2024, 5:07 PM +08:00

---
+++ game/game.py
@@ -0,0 +1,20 @@
+import random
+
+lv = int(input"Level:")
+ans = random(1,lv)
+
+while lv <= 0:
+    lv = int(input"Level:")
+
+guess = int(input("Guess:"))
+while lv <= 0:
+    guess = int(input("Guess:"))
+if guess == ans:
+    print("Just right!")
+elif guess > ans:
+    print("Too large!")
+else:
+    print("Too small!")
+
+
+
