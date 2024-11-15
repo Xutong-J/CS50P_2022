d8fc317bf62eedc8f9310e48514cfd5358cf066e
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-15 11:22:19 +0000

    Fri, Nov 15, 2024, 7:22 PM +08:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -7,9 +7,14 @@ if len(sys.argv) == 1:
 
 try:
     n = float(sys.argv[1])
-
-except (ValueError, requests.RequestException):
-    sys.exit("Command-line argument is not a number")
     r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
     data = r.json()
-    rate = data["bpi"]
+    print(json.dump(data))
+    bpi = data["bpi"]
+    usd = bpi["USD"]
+    rate = usd["rate_float"]
+    print(f"${amount:,.4f}")
+except (ValueError, requests.RequestException):
+    sys.exit("Command-line argument is not a number")
+
+
