5d33aab120c2622e3cc1add63f568c7d981ebb4c
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-18 09:08:14 +0000

    Mon, Nov 18, 2024, 5:08 PM +08:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,3 +1,14 @@
 from bank import value
 
+def test_hello():
+    assert value("hello") == '$0'
+    assert value("Hello, James") == '$0'
 
+
+def test_h():
+    assert value("how are you?") == '$20'
+    assert value("How do you do") == '$20'
+
+def test_other():
+    assert value("What do you do") == '$100'
+    assert value("Good morning") == '$100'
