d3e939488c5a51901b4e448cbb17122cf6984cc6
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-18 07:10:30 +0000

    Mon, Nov 18, 2024, 3:10 PM +08:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,7 +1,18 @@
-string = input("Input:")
-twttr =""
-for i in string:
-    if i not in ['a', 'e', 'i' ,'o', 'u', 'A', 'E', 'I', 'O', 'U']:
-        twttr = twttr + i
-print(twttr)
+def main():
+    word = input("Input:")
+    print(shorten(word))
+
+
+
+def shorten(word):
+    twttr =""
+    for i in word:
+        if i not in ['a', 'e', 'i' ,'o', 'u', 'A', 'E', 'I', 'O', 'U']:
+            twttr = twttr + i
+    return twttr
+
+
+if __name__ == "__main__":
+    main()
+
 
