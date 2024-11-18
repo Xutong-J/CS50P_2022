2efed098365f1f9135f6ffecc7b0d7253fb422a2
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-18 10:28:42 +0000

    Mon, Nov 18, 2024, 6:28 PM +08:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,3 +1,14 @@
 from plates import is_valid
 
-
+def test_long():
+    assert is_valid("AAAAA678") == False
+    assert is_valid("W") == False
+def test_starter():
+    assert is_valid("W3ERD") == False
+    assert is_valid("2ewrt") == False
+def test_middle():
+    assert is_valid("AAA22A") == False
+    assert is_valid("AAA222") == True
+def test_other():
+    assert is_valid(" sdf34")  == False
+    assert is_valid("AAA2,2")  == False
