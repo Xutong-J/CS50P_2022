6386011dd2524d674a1fade7093bd681ffd3d302
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-18 11:31:31 +0000

    Mon, Nov 18, 2024, 7:31 PM +08:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,1 +1,7 @@
-from fuel import 
+from fuel import convert, gauge
+
+def test_cnvrt():
+    assert convert('3/4') == 75
+    assert convert('1/4') == 25
+    assert convert('4/4') == 100
+    assert convert('0/4') == 0
