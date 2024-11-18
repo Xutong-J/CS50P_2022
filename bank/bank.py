6c9968d32a4dbf0e6f34835d4440898139d401cb
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-18 08:00:06 +0000

    Mon, Nov 18, 2024, 4:00 PM +08:00

--- bank/bank.py
+++ bank/bank.py
@@ -4,7 +4,12 @@ def main():
 
 
 def value(greeting):
-    if greeting[:4].lower() == "hello"
+    if greeting[:5].lower() == "hello":
+        return "$0"
+    elif greeting[0].lower() == 'h':
+        return '$20'
+    else:
+        return '$100'
 
 
 if __name__ == "__main__":
