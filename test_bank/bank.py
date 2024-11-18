1ce42eb5466d7392d598450d9d5bb2a891d022a4
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-18 09:25:36 +0000

    Mon, Nov 18, 2024, 5:25 PM +08:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,15 +1,15 @@
 def main():
     greeting = input("Greeting:")
-    print(value(greeting.strip()))
+    print('$'+str(value(greeting.strip())))
 
 
 def value(greeting):
     if greeting[:5].lower() == "hello":
-        return "$0"
+        return 0
     elif greeting[0].lower() == 'h':
-        return '$20'
+        return 20
     else:
-        return '$100'
+        return 100
 
 
 if __name__ == "__main__":
