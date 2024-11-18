8d8d5d117cdaf41b1b5fef6e9c68e9693b5a0723
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-18 11:06:35 +0000

    Mon, Nov 18, 2024, 7:06 PM +08:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -7,11 +7,19 @@ def main():
             pass
 
 def convert(fraction):
-    ...
+    x, y = fraction.split("/")  # 使用分隔符 '/' 分割字符串
+    return round(int(x) / int(y) * 100)
+
 
 
 def gauge(percentage):
-    ...
+    if percentage >= 99:
+        return 'F'
+    elif percentage <= 1:
+        return 'E'
+    else:
+        print(f"{rslt}%")
+        break
 
 
 if __name__ == "__main__":
