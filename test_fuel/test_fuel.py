3c7bccd97f412b1a53e1822fe23ca0ea0626eacc
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-18 11:39:10 +0000

    Mon, Nov 18, 2024, 7:39 PM +08:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -9,6 +9,14 @@ def test_cnvrt():
 
     with pytest.raises(ValueError):
         convert('1.5/3')
+    with pytest.raises(ValueError):
         convert('three/four')
     with pytest.raises(ZeroDivisionError):
         convert('4/0')
+def test_gauge():
+    assert gauge(100) == 'F'
+    assert gauge(99) == 'F'
+    assert gauge(0) == 'E'
+    assert gauge(1) == 'E'
+    assert gauge(75) == '75%'
+    assert gauge(25) == '25%'
