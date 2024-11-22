77fc3e72c7f4bb0cc3078df2ec21fcfa710514b5
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-22 10:18:55 +0000

    Fri, Nov 22, 2024, 6:18 PM +08:00

--- lines/lines.py
+++ lines/lines.py
@@ -4,6 +4,7 @@ if len(sys.argv) == 1:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 2:
     sys.exit("TToo many command-line arguments")
-if sys.argv[1]:
-    
+file_name = sys.argv[1]
+if file_name[-3:] == '.py'
+
 
