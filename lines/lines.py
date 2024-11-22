091b396b1a37c997adc6c5b1f81fa0497a7d28c8
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-22 10:26:10 +0000

    Fri, Nov 22, 2024, 6:26 PM +08:00

--- lines/lines.py
+++ lines/lines.py
@@ -3,8 +3,15 @@ import sys
 if len(sys.argv) == 1:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 2:
-    sys.exit("TToo many command-line arguments")
+    sys.exit("Too many command-line arguments")
 file_name = sys.argv[1]
-if file_name[-3:] == '.py'
-
+if file_name[-3:] == '.py':
+    try:
+        with open(file_name,'r') as f:
+            lines = f.readlines()
+        print(len(lines))
+    except FileNotFoundError:
+        sys.exit("File does not exist")
+else:
+    sys.exit("Not a Python file")
 
