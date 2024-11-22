4bf408ef1a245c97f0e13a8c544393dce7d950ae
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-22 10:16:38 +0000

    Fri, Nov 22, 2024, 6:16 PM +08:00

--- lines/lines.py
+++ lines/lines.py
@@ -2,5 +2,8 @@ import sys
 
 if len(sys.argv) == 1:
     sys.exit("Too few command-line arguments")
-    elif len(sys.argv) =
+elif len(sys.argv) > 2:
+    sys.exit("TToo many command-line arguments")
+if sys.argv[1]:
+    
 
