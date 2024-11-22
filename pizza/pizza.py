cc77dc36ca3bf94b15fe47571f3b139ca95edf55
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-22 11:03:50 +0000

    Fri, Nov 22, 2024, 7:03 PM +08:00

---
+++ pizza/pizza.py
@@ -0,0 +1,25 @@
+import sys
+import csv
+from tabulate import tabulate
+
+def main():
+    if len(sys.argv) == 1:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    file_name = sys.argv[1]
+    if file_name[-3:] == '.py':
+        try:
+            with open(file_name,'r') as f:
+                lines = f.readlines()
+                n = 0
+            for line in lines:
+                stripped_line = line.lstrip()
+                if stripped_line and not stripped_line.startswith("#"):
+                    n += 1
+
+            print(n)
+        except FileNotFoundError:
+            sys.exit("File does not exist")
+    else:
+        sys.exit("Not a Python file")
