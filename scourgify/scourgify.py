42b1e9e5d0e76b031ff77046d882f05dad02c155
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-23 09:13:45 +0000

    Sat, Nov 23, 2024, 5:13 PM +08:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -6,22 +6,23 @@ def main():
         sys.exit("Too few command-line arguments")
     if len(sys.argv) > 3:
         sys.exit("Too many command-line arguments")
-    file_name = sys.argv[1]
+    file_name, out_name = sys.argv[1], sys.argv[2]
     if file_name[-4:] == '.csv':
         try:
-            method2(file_name)
+            read_write(file_name,out_name)
         except FileNotFoundError:
             sys.exit("Could not read {file_name}")
     else:
         sys.exit("Not a CSV file")
 
 
-def read(file_name,out_name):
+def read_write(file_name,out_name):
     with open(file_name,'r') as f, open(out_name,'w') as o:
         reader = csv.DictReader(f)
         writer = csv.DictWriter(o)
     for row in reader:
         last,first = row["name"].strip().split(', ')
-        writer
+        writer.writeheader()
+        writer.writerow({'first_name': {first}, 'last_name':{last}, 'house': {row['house']}})
 
 
