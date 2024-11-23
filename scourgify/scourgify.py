1dded6ebf0dbe9c0b510c8333e9f1375b0a2d34d
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-23 08:54:12 +0000

    Sat, Nov 23, 2024, 4:54 PM +08:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -4,7 +4,7 @@ import sys
 def main():
     if len(sys.argv) == 1:
         sys.exit("Too few command-line arguments")
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 3:
         sys.exit("Too many command-line arguments")
     file_name = sys.argv[1]
     if file_name[-4:] == '.csv':
