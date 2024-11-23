3581aacb0f388c2bfcd20637054ba444508d4df1
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-23 09:20:38 +0000

    Sat, Nov 23, 2024, 5:20 PM +08:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -17,11 +17,14 @@ def main():
 
 
 def read_write(file_name,out_name):
+    students = []
     with open(file_name,'r') as f, open(out_name,'w') as o:
         reader = csv.DictReader(f)
         writer = csv.DictWriter(o)
     for row in reader:
         last,first = row["name"].strip().split(', ')
+        students.append({"first": first, "last": last, "home": row["home"]})
+for student in sorted(students, key=lambda student: student["first"]):
         writer.writeheader()
         writer.writerow({'first_name': {first}, 'last_name':{last}, 'house': {row['house']}})
 
