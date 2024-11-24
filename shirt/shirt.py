47931573edecb89003debfef4dc9f336404dc301
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-11-24 08:03:51 +0000

    Sun, Nov 24, 2024, 4:03 PM +08:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -7,10 +7,13 @@ def main():
         sys.exit("Too few command-line arguments")
     if len(sys.argv) > 3:
         sys.exit("Too many command-line arguments")
+
     in_path, out_path = sys.argv[1], sys.argv[2]
-    in_ext, out_ext =Path(in_path), Path(out_path)
-    if in_ext. != out_ext
-    if Path(img_in).suffix.lower() in ['.jpg', '.jpeg', '.png']:
+    in_ext, out_ext =Path(in_path).suffix, Path(out_path).suffix
+
+    if in_ext != out_ext:
+        sys.exit("Input and output have different extensions")
+    if in_ext.lower() in ['.jpg', '.jpeg', '.png']:
         try:
             read_write(img_in, img_out)
         except FileNotFoundError:
