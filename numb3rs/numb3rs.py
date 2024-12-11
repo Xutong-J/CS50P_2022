8b2c16be213a4ffd596d43a17da0e6c4a231cfac
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-11 08:32:06 +0000

    Wed, Dec 11, 2024, 4:32 PM +08:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -8,7 +8,7 @@ def main():
 
 def validate(ip):
     pattern = r"^(d{1,3})\.(d{1,3})\.(d{1,3})\.(d{1,3})$"
-    if matches:=re.search(pattern, ip.strip()):
+  1  if matches:=re.search(pattern, ip.strip()):
         for n in range(1,5):
             if 0 <= int(matches.group(n)) <= 255:
                 return "False"
