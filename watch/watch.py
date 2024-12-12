5a53b5ea781e8c94aac7f264fcf26c0e2771e3a3
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-12 02:03:52 +0000

    Thu, Dec 12, 2024, 10:03 AM +08:00

--- watch/watch.py
+++ watch/watch.py
@@ -2,6 +2,7 @@ import re
 import sys
 
 
+embed_code = "<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0?si=3gFhPlkne8Ps5wz9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"
 def main():
     print(parse(input("HTML: ")))
 
