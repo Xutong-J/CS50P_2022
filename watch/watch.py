74a8ffd26f3890a0e2be00e23a23996a9daec01d
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-12 02:27:51 +0000

    Thu, Dec 12, 2024, 10:27 AM +08:00

--- watch/watch.py
+++ watch/watch.py
@@ -2,16 +2,17 @@ import re
 import sys
 
 
-embed_code = '<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0?si=3gFhPlkne8Ps5wz9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+#embed_code = '<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0?si=3gFhPlkne8Ps5wz9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
 def main():
     print(parse(input("HTML: ")))
 
 
 def parse(s):
-    matches = re.search(r'^(?:<iframe)+.?scr="(.+)".*(?:</iframe>)&')
-
-
-...
+    matches = re.search(r'^(?:<iframe)+.?scr="(.+)".*(?:</iframe>)&', s.strip())
+    if matches:
+        return matches.group(1)
+        else:
+            return None
 
 
 if __name__ == "__main__":
