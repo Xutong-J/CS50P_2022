5ffea686529da27ae0b139d458e692c4d6beb8ac
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-12 09:20:49 +0000

    Thu, Dec 12, 2024, 5:20 PM +08:00

--- week7/working/test.py
+++ week7/working/test.py
@@ -1,6 +1,6 @@
 import re
 
 s = input("Time:")
-pattern = r"^([0-9\:\ ]+)(AM|PM) to ([0-9\:\ ]+)(AM|PM)$"
+pattern = r"^([0-9:\ ]+)(AM|PM) to ([0-9:\ ]+)(AM|PM)$"
 matches = re.search(pattern, s.strip())
 print(matches)
