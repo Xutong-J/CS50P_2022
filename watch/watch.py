285303a2b78db25ee22ea7ccd7331249d4b512fb
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-12 03:12:12 +0000

    Thu, Dec 12, 2024, 11:12 AM +08:00

--- watch/watch.py
+++ watch/watch.py
@@ -8,13 +8,10 @@ def main():
 
 
 def parse(s):
-    matches = re.search(r'src="(.+embed/).*([^"]+)"', s.strip())
+    matches = re.search(r'src="([^"]+)"', s.strip())
     if matches:
-        if ".youtube.com/embed/" in matches.group(1):
-            return f"https://youtu.be/{matches.group(2)}"
-        else:
-            return matches.group(1)+matches.group(2)
-
+        
+        return matches.group(1)
     else:
         return None
 
