326a80519dadf96e5e5b3c07ded3e09cb57c7dac
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-12 08:07:26 +0000

    Thu, Dec 12, 2024, 4:07 PM +08:00

--- week7/working/working.py
+++ week7/working/working.py
@@ -7,10 +7,12 @@ def main():
 
 
 def convert(s):
-    pattern = r"^[0-9: ]+(AM|PM)$"
+    pattern = r"^([0-9:]) (AM|PM) to ([0-9:]) (AM|PM)$"
     matches = re.search(pattern, s.strip())
     if matches:
-        
+        t1, t2 = int(matches.group(1)), int(matches.group(3))
+        flag1, flag2 = matches.group(2), matches.group(4)
+        for
 
 
 ...
