d003d7f2a2d7877c0b0b7188afdc3df8392bdae1
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-12 08:49:03 +0000

    Thu, Dec 12, 2024, 4:49 PM +08:00

--- week7/working/test_working.py
+++ week7/working/test_working.py
@@ -1,3 +1,11 @@
 from working import convert
 
-def test_no
+def test_no_m():
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("9:00 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("9 AM to 5:00 PM") == "09:00 to 17:00"
+    assert convert("10：30 PM to 8 AM") == "22:30 to 08:00"
+
+def test_VE():
+    assert convert("9:60 AM to 5:60 PM") == "09:00 to 17:00"
+
