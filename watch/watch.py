32c8b26cff2b51bb7f3586a0134158aeb874446f
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-12 03:47:11 +0000

    Thu, Dec 12, 2024, 11:47 AM +08:00

--- watch/watch.py
+++ watch/watch.py
@@ -1,5 +1,4 @@
 import re
-import sys
 
 
 #embed_code = '<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0?si=3gFhPlkne8Ps5wz9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
@@ -13,10 +12,6 @@ def parse(s):
         param = re.search(r"http(?:s)*:\/\/(?:www\.)*youtube\.com\/embed\/([a-z_A-Z_0-9]+)", matches.group(1))
         if param:
             return "https://youtu.be/" + param.group(1)
-        else:
-            return matches.group(1)
-    else:
-        return None
 
 
 if __name__ == "__main__":
