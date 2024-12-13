15c4fe9a3ecd37bc2f2297e2a76962a507c36ea6
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-13 02:40:02 +0000

    Fri, Dec 13, 2024, 10:40 AM +08:00

--- week7/working/test_working.py
+++ week7/working/test_working.py
@@ -10,10 +10,19 @@ def test_no_m():
 def test_VE():
     with pytest.raises(ValueError):
         convert("9：60 AM to 5：60 PM")
+    with pytest.raises(ValueError):
         convert("9 AM - 5 PM")
-        convert("09:00 AM - 17:00 PM")
+    with pytest.raises(ValueError):
+       convert("09:00 AM - 17:00 PM")
+    with pytest.raises(ValueError):
         convert("9 AM 5:00 PM")
-        convert("13：60 AM to 5：20 PM")
+    with pytest.raises(ValueError):
+        convert("13: 60 AM to 5: 20 PM")
+    with pytest.raises(ValueError):
+        convert("5AM to 9PM")
+    with pytest.raises(ValueError):
+        convert("11:00 AM 5:20 PM")
+
 
 def test_nomal():
     assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
