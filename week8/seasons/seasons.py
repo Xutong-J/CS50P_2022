424a7dd042e809771f0e5690dae54718b5210b6f
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-15 11:25:15 +0000

    Sun, Dec 15, 2024, 7:25 PM +08:00

--- week8/seasons/seasons.py
+++ week8/seasons/seasons.py
@@ -4,8 +4,13 @@ import re
 
 
 def main():
+    birthday = get_birth()
+
+
+
+def get_birth():
     birth = input("Date of Birth: ")
-    matches = re.search(r"^(\d{4})-(\d{2})-(\d{2})$")
+    matches = re.search(r"^(\d{4})-(\d{2})-(\d{2})$", birth)
     if matches:
         year = matches.group(1)
         month = matches.group(2)
@@ -13,9 +18,7 @@ def main():
     else:
         sys.exit()
 
-
-
-...
+    return date(year, month, day)
 
 
 if __name__ == "__main__":
