41da0b967eb9a4bbd34d9419fb2643b1ac2e07d9
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-15 11:22:15 +0000

    Sun, Dec 15, 2024, 7:22 PM +08:00

--- week8/seasons/seasons.py
+++ week8/seasons/seasons.py
@@ -5,7 +5,13 @@ import re
 
 def main():
     birth = input("Date of Birth: ")
-    matches = re.search(r"^\d{4}-\d{2}-\d{2}$")
+    matches = re.search(r"^(\d{4})-(\d{2})-(\d{2})$")
+    if matches:
+        year = matches.group(1)
+        month = matches.group(2)
+        day = matches.grop(3)
+    else:
+        sys.exit()
 
 
 
