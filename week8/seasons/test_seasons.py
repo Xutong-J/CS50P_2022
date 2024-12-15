1787e9c61bb1911aec2956175e32be3be98d3256
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-15 12:15:19 +0000

    Sun, Dec 15, 2024, 8:15 PM +08:00

--- week8/seasons/test_seasons.py
+++ week8/seasons/test_seasons.py
@@ -1,7 +1,20 @@
 from seasons import get_birth, calculate
 from datetime import date
 import pytest
+from inflect import engine
 
-assert get_birth("2024-12-14") == date(2024, 12, 14)
-with pytest.raises(SystemExit) as pytest_e:
-    
+def test_get_birth():
+    assert get_birth("2024-12-14") == date(2024, 12, 14)
+
+    with pytest.raises(SystemExit):
+        get_birth("20241214")
+    with pytest.raises(SystemExit):
+        get_birth("2024,12,14")
+    with pytest.raises(SystemExit):
+        get_birth("2024 12 14")
+
+def test_calculate()
+    p = engine()
+    assert calculate(date(2024,12,14),date(2024,12,15)) == f"{p.number_to_words(1440, andword="")} minutes"
+    assert calculate(date(2023,12,14),date(2024,12,15)) == "Five hundred twenty-seven thousand forty minutes"
+    assert calculate(date(2023,12,14),date(2022,12,15)) == "Five hundred twenty-five thousand, six hundred minutes"
