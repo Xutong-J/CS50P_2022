2b224860aae0c0592e70c1cd9ee7a20df56e7166
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-16 07:59:33 +0000

    Mon, Dec 16, 2024, 3:59 PM +08:00

--- week8/shirtificate/shirtificate.py
+++ week8/shirtificate/shirtificate.py
@@ -10,7 +10,7 @@ class PDF(FPDF):
 def main():
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
-    pdf.set_font("Arial", size=15)
+    pdf.set_font("Helvetica", size=30)
     pdf.cell(200, 10, txt="CS50 Shirtificate", ln=True)
     pdf.image("./shirtificate.png", x=None, y=0, w=190, h=280, keep_aspect_ratio=True)
     pdf.output("example.pdf")
