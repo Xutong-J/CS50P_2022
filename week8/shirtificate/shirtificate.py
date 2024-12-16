ec0f225149a160e08ed06dfbc46b86abf7d03034
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-16 08:07:04 +0000

    Mon, Dec 16, 2024, 4:07 PM +08:00

--- week8/shirtificate/shirtificate.py
+++ week8/shirtificate/shirtificate.py
@@ -1,20 +1,40 @@
 from fpdf import FPDF
 
 
-class PDF(FPDF):
-    ...
+class Shirtificate(FPDF):
+    def header(self):
+        self.set_font('Arial', 'B', 16)
+        self.cell(0, 10, 'CS50 Shirtificate', 0, 1, 'C')
 
+    def footer(self):
+        self.set_y(-15)
+        self.set_font('Arial', 'I', 8)
+        self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'C')
 
 
 
 def main():
-    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    # 创建PDF对象
+    pdf = Shirtificate()
     pdf.add_page()
-    pdf.set_font("Helvetica", size=50)
-    pdf.cell(200, 10, txt="CS50 Shirtificate",align='C')
-    pdf.image("./shirtificate.png", x=None, y=0, w=190, h=280, keep_aspect_ratio=True)
-    pdf.cell(200, 80, txt="Jiang Xianfu took CS50",align='C')
-    pdf.output("example.pdf")
+    pdf.set_font('Arial', 'B', 12)
+
+    # 获取用户输入的名字
+    name = input("Please enter your name: ")
+
+    # 添加标题
+    pdf.header()
+
+    # 添加图片
+    pdf.image('shirtificate.png', x=(pdf.w - 100) / 2, y=50, w=100)
+
+    # 添加用户名字
+    pdf.set_font('Arial', 'B', 18)
+    pdf.set_text_color(255, 255, 255)  # 白色文本
+    pdf.cell(0, 10, name, 0, 0, 'C')
+
+    # 输出PDF文件
+    pdf.output('shirtificate.pdf')
 
 if __name__ == "__main__":
     main()
