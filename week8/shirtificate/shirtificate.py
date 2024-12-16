62e3ae144eb1e689f5bf96564d26262f41fb64f0
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-16 07:51:21 +0000

    Mon, Dec 16, 2024, 3:51 PM +08:00

--- week8/shirtificate/shirtificate.py
+++ week8/shirtificate/shirtificate.py
@@ -12,7 +12,7 @@ def main():
     pdf.add_page()
     pdf.set_font("Arial", size=15)
     pdf.cell(200, 10, txt="Hello, World!", ln=True)
-    pdf.image("image.jpg", x=10, y=10, w=80)
+    pdf.image("./shirtificate.png", x=10, y=10, w=80)
     pdf.output("example.pdf")
 
 if __name__ == "__main__":
