7b9afa23090bf51408f3d200cef84381435d2a72
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-16 02:05:29 +0000

    Mon, Dec 16, 2024, 10:05 AM +08:00

--- week8/jar/jar.py
+++ week8/jar/jar.py
@@ -1,9 +1,11 @@
 class Jar:
     def __init__(self, capacity=12):
+        if capacity < 0:
+            raise ValueError
         ...
 
     def __str__(self):
-        ...
+        return 
 
     def deposit(self, n):
         ...
