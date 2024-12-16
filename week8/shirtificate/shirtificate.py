96c668dedbd8d3aa280ebba147e093745b50fad8
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-16 07:45:53 +0000

    Mon, Dec 16, 2024, 3:45 PM +08:00

--- week8/shirtificate/shirtificate.py
+++ week8/shirtificate/shirtificate.py
@@ -8,7 +8,12 @@ class PDF(FPDF):
 
 
 def main():
-    ...
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=15)
+    pdf.cell(200, 10, txt="Hello, World!", ln=True)
+    pdf.image("image.jpg", x=10, y=10, w=80)
+    pdf.output("example.pdf")
 
 if __name__ == "__main__":
     main()
