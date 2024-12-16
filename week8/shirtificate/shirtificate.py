51272cca58b302f87da53e223bc940b2b265e8a7
Xutong-J <71955439+Xutong-J@users.noreply.github.com>
2024-12-16 08:10:38 +0000

    Mon, Dec 16, 2024, 4:10 PM +08:00

--- week8/shirtificate/shirtificate.py
+++ week8/shirtificate/shirtificate.py
@@ -3,35 +3,26 @@ from fpdf import FPDF
 
 class Shirtificate(FPDF):
     def header(self):
-        self.set_font('Arial', 'B', 16)
+        self.set_font('Arial', 'B', 40)
         self.cell(0, 10, 'CS50 Shirtificate', 0, 1, 'C')
-
-    def footer(self):
-        self.set_y(-15)
-        self.set_font('Arial', 'I', 8)
-        self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'C')
-
 
 
 def main():
     # 创建PDF对象
     pdf = Shirtificate()
     pdf.add_page()
-    pdf.set_font('Arial', 'B', 12)
-
-    # 获取用户输入的名字
-    name = input("Please enter your name: ")
+    pdf.set_font('Arial', 'B', 20)
 
     # 添加标题
     pdf.header()
 
     # 添加图片
-    pdf.image('shirtificate.png', x=(pdf.w - 100) / 2, y=50, w=100)
+    pdf.image('shirtificate.png', x=None, y=0, w=190, h=280)
 
     # 添加用户名字
     pdf.set_font('Arial', 'B', 18)
     pdf.set_text_color(255, 255, 255)  # 白色文本
-    pdf.cell(0, 10, name, 0, 0, 'C')
+    pdf.cell(0, 10, "Jiang Xianfu took CS50", 0, 0, 'C')
 
     # 输出PDF文件
     pdf.output('shirtificate.pdf')
